459e3cffdf7a73bc325695b13846a3c3aeb52fdc
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-11 12:34:11 -0500

    feat: llene el codigo de la clase vehiculo con sus atributos y metodos

--- src/main/java/Ejercicio2/Vehiculo.java
+++ src/main/java/Ejercicio2/Vehiculo.java
@@ -1,4 +1,13 @@
 package Ejercicio2;
 
 public class Vehiculo {
+    public String marca;
+    public String modelo;
+    public int velocidadActual;
+
+    public void mostrarEstado() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad Actual: " + velocidadActual + " km/h");
+    }
 }
