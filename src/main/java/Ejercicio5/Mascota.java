ec561a703d2e04cd12645bd49d0c20bf9f84bfa2
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-12 01:16:10 -0500

    feat: llene la clase Mascota con sus atributos y metodo mostrarMascota

--- src/main/java/Ejercicio5/Mascota.java
+++ src/main/java/Ejercicio5/Mascota.java
@@ -1,4 +1,14 @@
 package Ejercicio5;
 
 public class Mascota {
+    public String nombre;
+    public String especie;
+    public int edad;
+
+    public void mostrarMascota() {
+        System.out.println("--- Información de la Mascota ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad + " años");
+    }
 }
