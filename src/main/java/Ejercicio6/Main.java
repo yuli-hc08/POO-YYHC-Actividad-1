957c04563539414c7fa04017abfe959b3f4c6a33
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-12 18:20:52 -0500

    elimine el import para que el ejercicio6 sea independiente

--- src/main/java/Ejercicio6/Main.java
+++ src/main/java/Ejercicio6/Main.java
@@ -1,12 +1,10 @@
 package Ejercicio6;
 
-import Ejercicio1.Estudiante;
-
 public class Main {
     public static void main(String[] args) {
         Estudiante estudiante1 = new Estudiante();
         estudiante1.nombre = "Carlos Gómez";
-        estudiante1.codigo = "EST-2026-01";
+        estudiante1.codigo = "7502610128";
         estudiante1.semestre = 3;
 
         estudiante1.mostrarInfo();
