66c66f571133f1f794aa35d4c7524509a8c1cfdd
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-13 22:00:05 -0500

    feat: agregue el metodo frenanr evitando velocidades negativas y valido en Main

--- src/main/java/Ejercicio12/Vehiculo.java
+++ src/main/java/Ejercicio12/Vehiculo.java
@@ -4,4 +4,16 @@ public class Vehiculo {
     public String marca;
     public String modelo;
     public int velocidad;
+
+    public void frenar() {
+        if (this.velocidad >= 10) {
+            this.velocidad -= 10;
+        } else {
+            this.velocidad = 0;
+        }
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Vehículo: " + marca + " " + modelo + " | Velocidad actual: " + velocidad + " km/h");
+    }
 }
