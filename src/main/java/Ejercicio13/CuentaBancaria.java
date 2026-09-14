1aaffeb7de9f9d7efbffc562d7fd8db7e91d6c8d
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-13 22:39:30 -0500

    feat: agrego el metodo consignar y valido el incremento de saldo en Main

--- src/main/java/Ejercicio13/CuentaBancaria.java
+++ src/main/java/Ejercicio13/CuentaBancaria.java
@@ -4,4 +4,17 @@ public class CuentaBancaria {
     public String numeroCuenta;
     public String titular;
     public double saldo;
+
+    public void consignar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println("Consignación exitosa: $" + valor);
+        } else {
+            System.out.println("El valor a consignar debe ser mayor a 0.");
+        }
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Cuenta: " + numeroCuenta + " | Titular: " + titular + " | Saldo actual: $" + saldo);
+    }
 }
