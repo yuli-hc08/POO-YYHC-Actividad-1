794cfc4e57deffe2ff58a4db276c2060e3911c0d
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-15 08:50:07 -0500

    feat: agrego el metodo cambiarSemestre y valido la actualizacion en Main

--- src/main/java/Ejercicio17/Estudiante.java
+++ src/main/java/Ejercicio17/Estudiante.java
@@ -4,4 +4,17 @@ public class Estudiante {
     public String nombre;
     public String carrera;
     public int semestre;
+
+    public void cambiarSemestre(int nuevoSemestre) {
+        if (nuevoSemestre > 0) {
+            this.semestre = nuevoSemestre;
+            System.out.println("Semestre actualizado correctamente a: " + nuevoSemestre);
+        } else {
+            System.out.println("Error: El semestre debe ser un número positivo.");
+        }
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Estudiante: " + nombre + " | Carrera: " + carrera + " | Semestre actual: " + semestre);
+    }
 }
