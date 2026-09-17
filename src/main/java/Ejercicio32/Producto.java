0d31ca4aa2ec3f533d53da88e8d7329ebc6f5230
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 17:13:23 -0500

    feat: creo la clase Producto con su metodo fabrica estatico

---
+++ src/main/java/Ejercicio32/Producto.java
@@ -0,0 +1,18 @@
+package Ejercicio32;
+
+public class Producto {
+    public String nombre;
+    public double precio;
+    public int cantidad;
+
+    public Producto(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    // Método fábrica estático
+    public static Producto crearProductoBasico(String nombre) {
+        return new Producto(nombre, 1000.0, 1);
+    }
+}
