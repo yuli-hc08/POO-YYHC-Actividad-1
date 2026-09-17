5e441fae1e046547b3a0e9d1c7934fc5d1f44509
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 16:16:00 -0500

    feat: creo la clase Libro con constructor con parametros

---
+++ src/main/java/Ejercicio24/Libro.java
@@ -0,0 +1,14 @@
+package Ejercicio24;
+
+public class Libro {
+    public String titulo;
+    public String autor;
+    public int paginas;
+
+    // Constructor con parámetros
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+}
