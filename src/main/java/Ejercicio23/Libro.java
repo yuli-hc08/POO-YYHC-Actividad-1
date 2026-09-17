a75cfa70c9fcb69791874436ffc6fffa6d31e4b3
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 16:11:28 -0500

    feat: creo la clase libro con sus atributos y constructor vacio

---
+++ src/main/java/Ejercicio23/Libro.java
@@ -0,0 +1,11 @@
+package Ejercicio23;
+
+public class Libro {
+    public String titulo;
+    public String autor;
+    public int paginas;
+
+    // Constructor vacío
+    public Libro() {
+    }
+}
