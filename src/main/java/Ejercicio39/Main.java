762a637937a50dfd9f912519764c0824000a3604
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 17:51:46 -0500

    feat: instancio Cancion y ejecuto mostrarInfo en Main

---
+++ src/main/java/Ejercicio39/Main.java
@@ -0,0 +1,9 @@
+package Ejercicio39;
+
+public class Main {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion("Bohemian Rhapsody", "Queen", 5.55);
+
+        miCancion.mostrarInfo();
+    }
+}
