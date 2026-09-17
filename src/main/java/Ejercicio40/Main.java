30d01f6c44078024e9844d84a029ee35d8d8e8df
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 17:54:28 -0500

    feat: instancio Pelicula y ejecuto mostrarFicha en Main

---
+++ src/main/java/Ejercicio40/Main.java
@@ -0,0 +1,9 @@
+package Ejercicio40;
+
+public class Main {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("Interstellar", "Ciencia Ficción", 169);
+
+        miPelicula.mostrarFicha();
+    }
+}
