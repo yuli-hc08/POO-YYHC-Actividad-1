4ddc4bf1259ed26141b759e8e12f30cd20fa891e
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 20:45:49 -0500

    feat: instancio NotaAcademica y pruebo el calculo del promedio en Main

---
+++ src/main/java/Ejercicio57/Main.java
@@ -0,0 +1,10 @@
+package Ejercicio57;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== EJERCICIO 57: NOTA ACADÉMICA ===\n");
+
+        NotaAcademica notaPoo = new NotaAcademica("Programación Orientada a Objetos", 4.5, 3.8, 4.2);
+        notaPoo.mostrarReporte();
+    }
+}
