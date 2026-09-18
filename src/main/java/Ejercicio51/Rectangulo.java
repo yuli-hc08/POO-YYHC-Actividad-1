f3a5cee781d891d36d4e20269517592bb38139f0
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 19:28:59 -0500

    feat: creo la clase Rectangulo con atributos base, altura y metodos de calculo

---
+++ src/main/java/Ejercicio51/Rectangulo.java
@@ -0,0 +1,27 @@
+package Ejercicio51;
+
+public class Rectangulo {
+    public double base;
+    public double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+
+    public void mostrarResultados() {
+        System.out.println("=== DATOS DEL RECTÁNGULO ===");
+        System.out.println("Base: " + base);
+        System.out.println("Altura: " + altura);
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro());
+    }
+}
