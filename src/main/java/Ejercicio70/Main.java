0bf7e32481c80136054b2a4aeaf9bd2a7f506f00
yulieth <yherrerac2@unicartagena.edu.co>
2026-09-17 22:17:26 -0500

    creo la estructura inicial de Main para responder los 4 puntos del Ejercicio 70

---
+++ src/main/java/Ejercicio70/Main.java
@@ -0,0 +1,8 @@
+package Ejercicio70;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== EJERCICIO 70: EXPLICACIÓN ESCRITA ===\n");
+        // Estructura lista para redactar los 4 puntos requeridos
+    }
+}
